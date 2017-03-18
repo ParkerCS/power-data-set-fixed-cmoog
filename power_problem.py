981cf2a9e10787fd5090b47768d58a999a8e0762
Charlie <cmoog@fwparker.org>
2017-03-18 11:29:05 -0500

    //

--- power_problem.py
+++ power_problem.py
@@ -22,9 +22,89 @@ FOR ALL THE RATES, ONLY USE THE BUNDLED VALUES (NOT DELIVERY).  This rate includ
 
 #1  What is the average residential rate for YOUR zipcode? You will need to read the power_data into your program to answer this.  (7pts)
 
+import matplotlib.pyplot
+import csv
+import statistics
+
+def selection_sort(list):
+    for pos in range(len(list)):
+        min_pos = pos
+        for sort_pos in range(min_pos, len(list)):
+            if list[sort_pos][0] < list[min_pos][0]:
+                min_pos = sort_pos
+        temp = list[pos]
+        list[pos] = list[min_pos]
+        list[min_pos] = temp
+    return list
+
+def binary_search(input_key, list):
+    key = input_key.upper()
+    lower_bound = 0
+    upper_bound = len(list) - 1
+    found = False
+    # loop until we find item or the bounds meet
+    while lower_bound <= upper_bound and not found:
+        # find the middle
+        middle_position = (lower_bound + upper_bound) // 2
+        # figure out if we need to move up or down
+        if list[middle_position][0] < key:
+            lower_bound = middle_position + 1
+        elif list[middle_position][0] > key:
+            upper_bound = middle_position - 1
+        else:
+            found = True
+    return middle_position
+
+with open("power_data.csv", 'r') as f: ## the reader we usually use wasn't working correctly so I found this online
+    reader = csv.reader(f)
+    your_list = list(reader)
+power_data = your_list
+
+
+headers = power_data[0]
+power_data.remove(power_data[0])
+'''
+power_data = sorted(power_data, key=lambda x: x[4])  # found this online
+index = len(power_data)-1
+while power_data[index][4] != "Bundled":
+    power_data.remove(power_data[index])
+    index -= 1
+'''
+power_data = sorted(power_data, key=lambda x: x[0])
+print("The average residential rate for electricity in 60614 is", power_data[binary_search("60614", power_data)][8], "dollars per kWh.")
+
 #2 What is the MEDIAN rate for all BUNDLED RESIDENTIAL rates in Illinois? Use the data you extracted to check all "IL" zipcodes to answer this. (10pts)
+il_bund_res = []
+for i in range(len(power_data)):
+    if power_data[i][3] == "IL" and power_data[i][4] == "Bundled":
+        il_bund_res.append(power_data[i][8])
+print(il_bund_res)
+print("The median rate for bundled resident in IL is", statistics.median(il_bund_res))
+'''
+##remove all delivery lines (only keeps bundled)
+for item in power_data:
+    if item[4] == "Delivery":
+        power_data.remove(item)
+'''
 
 #3 What city in Illinois has the lowest residential rate?  Which has the highest?  You will need to go through the database and compare each value for this one. Then you will need to reference the zipcode dataset to get the city.  (15pts)
+il_bund_res = []
+for i in range(len(power_data)):
+    if power_data[i][3] == "IL" and power_data[i][4] == "Bundled":
+        il_bund_res.append(power_data[i])
+
+il_bund_res = sorted(il_bund_res, key=lambda x: x[8])
+lowest_rate_zip = il_bund_res[0][0]
+highest_rate_zip = il_bund_res[len(il_bund_res)-1][0]
+
+with open("free-zipcode-database-Primary.csv", 'r') as f: ## the reader we usually use wasn't working correctly so I found this online
+    reader = csv.reader(f)
+    zip_code_data = list(reader)
+
+zip_code_data = sorted(zip_code_data, key=lambda x: x[0])
+
+print("The city with the lowest residential rate in IL is", zip_code_data[binary_search(lowest_rate_zip, zip_code_data)][2])
+print("The city with the highest residential rate in IL is", zip_code_data[binary_search(highest_rate_zip, zip_code_data)][2])
 
 
 #FOR #4  CHOOSE ONE OF THE FOLLOWING TWO PROBLEMS. The first one is easier than the second.
