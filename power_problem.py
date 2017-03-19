8879773271aeb1d31fd2207b91b3a5ab85704b0f
Charlie <cmoog@fwparker.org>
2017-03-19 11:14:04 -0500

    //

--- power_problem.py
+++ power_problem.py
@@ -22,7 +22,7 @@ FOR ALL THE RATES, ONLY USE THE BUNDLED VALUES (NOT DELIVERY).  This rate includ
 
 #1  What is the average residential rate for YOUR zipcode? You will need to read the power_data into your program to answer this.  (7pts)
 
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import csv
 import statistics
 
@@ -78,8 +78,8 @@ il_bund_res = []
 for i in range(len(power_data)):
     if power_data[i][3] == "IL" and power_data[i][4] == "Bundled":
         il_bund_res.append(power_data[i][8])
-print(il_bund_res)
-print("The median rate for bundled resident in IL is", statistics.median(il_bund_res))
+
+print("The median rate for bundled residents in IL is", statistics.median(il_bund_res))
 '''
 ##remove all delivery lines (only keeps bundled)
 for item in power_data:
@@ -94,6 +94,7 @@ for i in range(len(power_data)):
         il_bund_res.append(power_data[i])
 
 il_bund_res = sorted(il_bund_res, key=lambda x: x[8])
+print("LOOK AT THIS IT SHOULD BE:", il_bund_res[0][8])
 lowest_rate_zip = il_bund_res[0][0]
 highest_rate_zip = il_bund_res[len(il_bund_res)-1][0]
 
@@ -111,4 +112,44 @@ print("The city with the highest residential rate in IL is", zip_code_data[binar
 #4  (Easier) USING ONLY THE ZIP CODE DATA... Make a scatterplot of all the zip codes in Illinois according to their Lat/Long.  Make the marker size vary depending on the population contained in that zip code.  Add an alpha value to the marker so that you can see overlapping markers.
 
 #4 (Harder) USING BOTH THE ZIP CODE DATA AND THE POWER DATA... Make a scatterplot of all zip codes in Illinois according to their Lat/Long.  Make the marker red for the top 25% in residential power rate.  Make the marker yellow for the middle 25 to 50 percentile. Make the marker green if customers pay a rate in the bottom 50% of residential power cost.  This one is very challenging.  You are using data from two different datasets and merging them into one.  There are many ways to solve. (20pts)
+def percentiles(list):
+    selection_sort(list)
+    print(list)
 
+long_data = []
+lat_data = []
+zips = []
+color_list = []
+size = []
+il_bund_res = sorted(il_bund_res, key=lambda x: x[8])
+res_rate = []
+for i in range(len(zip_code_data)):
+    if zip_code_data[i][3] == "IL":
+        long_data.append(zip_code_data[i][6])
+        lat_data.append(zip_code_data[i][5])
+        zips.append((zip_code_data[i][0]))
+        res_rate.append(il_bund_res[binary_search(zip_code_data[i][0], il_bund_res)][8])
+
+for j in range(len(res_rate)):
+    if j / len(res_rate) > .75:
+        color_list.append("red")
+    elif j / len(res_rate) > .5:
+        color_list.append("yellow")
+    else:
+        color_list.append("green")
+
+print(res_rate)
+print(selection_sort(res_rate))
+#for k in range(len(res_rate)):
+
+plt.figure(figsize=[4.7,6.5], tight_layout=True)
+my_plot = plt.scatter(long_data,lat_data, color=color_list)
+
+
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.title("Illinois Zip Codes - Scaled by Residential Electricity Rate")
+
+#plt.ylim([float(min(lat))- 5, float(max(long_data)) + 5])
+
+plt.show()
